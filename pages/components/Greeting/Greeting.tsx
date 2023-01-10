28bcd2f0c3a5d1574291d7d9dde433da52ee2a5f
Pat Ranile <dev.fatimaranile@gmail.com>
2023-01-11 01:26:10 +0800

    Install styled-component library. Create sample component.

---
+++ pages/components/Greeting/Greeting.tsx
@@ -0,0 +1,11 @@
+import { Title, Wrapper } from "./Greeting.style";
+
+const Greeting = () => {
+    return (
+        <Wrapper>
+            <Title>Styled Component</Title>
+        </Wrapper>
+    )
+};
+
+export default Greeting;
