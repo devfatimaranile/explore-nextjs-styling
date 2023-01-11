5582da47a56744df6afde3fbe30ceb5ea80099e1
Pat Ranile <dev.fatimaranile@gmail.com>
2023-01-11 19:26:22 +0800

    enable ssr mode of the style-component library.

--- pages/components/Progress/Circular/Circular.style.tsx
+++ pages/components/Progress/Circular/Circular.style.tsx
@@ -2,4 +2,7 @@ import styled from "styled-components";
 import { Progress } from "antd";
 
 export const CircleProgress = styled(Progress)`
+  .ant-progress-text {
+    color: white !important;
+  }
 `;
